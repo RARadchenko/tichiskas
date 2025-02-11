d8161c2ebd90a6e7ab92b02726920527e99351ae
MumuCarrot <ivan.kovalenko8080@gmai.com>
2025-02-11 16:15:28 +0100

    Add TaskLoader and TaskOption classes implementing their respective interfaces

---
+++ src/classes/taskloader.class.ts
@@ -0,0 +1,7 @@
+import { ITaskLoader } from "../interfaces/taskloader.interface";
+
+/**
+ * TaskLoader class that implements the ITaskLoader interface.
+ * Used to load and manage tasks.
+ */
+export class TaskLoader extends ITaskLoader {}
---
+++ src/classes/taskoption.class.ts
@@ -0,0 +1,7 @@
+import { ITaskOption } from "../interfaces/taskoption.interface";
+
+/**
+ * Класс TaskOption, реализующий интерфейс ITaskOption.
+ * Используется для работы с параметрами задачи.
+ */
+export class TaskOption extends ITaskOption {}
