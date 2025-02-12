59f4bdeb1a06d37d6104a342cf51f2ffe9604249
MumuCarrot <ivan.kovalenko8080@gmai.com>
2025-02-12 19:45:37 +0100

    Add task management methods to TaskComponent for editing, deleting, and finding tasks

--- src/task/task.component.ts
+++ src/task/task.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { TaskManager } from "../classes/taskmanager.class";
 import { ITaskLoader } from "../interfaces/taskloader.interface";
+import { ITask } from "../interfaces/task.interface";
+import { TaskStatus } from "../enums/taskstatus.enum";
 
 @Component({
     selector: 'task-comp',
@@ -11,5 +13,25 @@ export class TaskComponent {
 
     addTask(task: ITaskLoader) {
         this.taskManager.AddTask(task);
+    }
+
+    editTask(taskID: number, status: TaskStatus) {
+        this.taskManager.EditTask(taskID, status);
+    }
+
+    deleteTask(taskID: number) {
+        this.taskManager.DeleteTask(taskID);
+    }
+
+    findTaskByID(taskID: number): ITask {
+        return this.taskManager.FindTask(taskID);
+    }
+
+    findTaskByTitle(taskTitle: string): ITask {
+        return this.taskManager.FindTask(taskTitle);
+    }
+
+    findTaskByStatus(taskStatus: TaskStatus): ITask {
+        return this.taskManager.FindTask(taskStatus);
     }
 }
