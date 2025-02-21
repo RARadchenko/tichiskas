dcdc95a4a7901d16b76b926f33cc47a2c94fbd2e
Rostyslav <romantsev505@gmail.com>
2025-02-21 18:54:17 +0200

    Hotfix

--- src/classes/task.class.ts
+++ src/classes/task.class.ts
@@ -2,7 +2,7 @@ import { TaskStatus } from "../enums/taskstatus.enum";
 import { ITask } from "../interfaces/task.interface";
 
 export class Task extends ITask {
-    constructor() {
-        super();
+    constructor(title: string = "", dedescription: string = "", deadline: number = 0, status: TaskStatus = 3) {
+        super(title, dedescription, deadline, status);
     }
 }
