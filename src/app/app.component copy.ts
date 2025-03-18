efc3ecaf5ccccbd816c7997738117acb4dac9765
vanichka <138333895+MumuCarrot@users.noreply.github.com>
2025-03-18 18:35:15 +0100

    Add header component with styling and integrate into app layout

--- src/app/app.component copy.ts
+++ src/app/app.component copy.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeader } from "./header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppHeader],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { AppHeader } from "./header.component";
 
 @Component({
     selector: 'app-comp',
     standalone: true,
+    imports: [AppHeader],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
